0ec5ec1fcb8af2bc0992ac1fc8ded671613763aa
Eduardo Camacho Díaz <fgada200@gmail.com>
2022-10-19 13:39:14 +0100

    Initial comit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        2.- Desarrolla una segunda aplicación en Java que abra el número de ventanas del navegador de tu
+        elección que indique el usuario. Puede indicarlo como entrada de teclado o como argumento del
+        ejecutable. Los procesos deben terminar 500 ms después de arrancar. Cada proceso debe indicar la
+        hora de inicio y la hora de finalización. (3 puntos)
+        */
+
+        // Página con información relacionada al ejercicio. (Fuentes)
+        // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/ProcessBuilder.html
+        // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Process.html
+
+    }
+}
